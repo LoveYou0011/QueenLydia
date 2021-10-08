d521ca73dac0413e064bb4fa4071104563e2c5fc
LoveYou0011 <91460318+LoveYou0011@users.noreply.github.com>
2021-10-08 19:10:47 +0530

    Create git.js

---
+++ plugins/git.js
@@ -0,0 +1,29 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
+
+Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    
+   
+  r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/03/Pikachu-wallpaper-HD-dowload.jpg";
+    
+    
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*My creator is Ameer Suhail*
+*Creator number : wa.me/917909139146?text=Hi%20MonkSer%20😁.%20*
+*Bot setting video : https://youtu.be/_D4ZYuUSXjs*
+*Githublink (Setup)  :    https://github.com/LoveYou0011/QueenLydia*
+*Audio commads :   https://github.com/LoveYou0011/QueenLydia/tree/master/uploads*
+*Sticker commads : https://github.com/LoveYou0011/QueenLydia/tree/master/uploads*
+`}) 
+
+}));
